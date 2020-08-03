e887cf3230915e142fd15a80b50760a273959681
Sebag <octhave@gmail.com>
2020-08-03 18:18:21 +0200

    Add hello world + app class

---
+++ app/src/main/java/com/sebag/florent/cleanbaseapp/MyApp.kt
@@ -0,0 +1,13 @@
+package com.sebag.florent.cleanbaseapp
+
+import android.app.Application
+import android.util.Log
+
+class MyApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        Log.i("gnah", "coucou")
+    }
+
+}
